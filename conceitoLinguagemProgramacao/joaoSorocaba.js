6446d75ae06839e5aa634bb8f92f6d1c85a1702d
BRYAN PRINZ DA SILVA <bryan_prinz-silva@estudante.sesisenai.org.br>
2024-06-11 09:05:35 -0300

    Versão 1.0.1

---
+++ conceitoLinguagemProgramacao/joaoSorocaba.js
@@ -0,0 +1,42 @@
+let res1 = document.getElementById('res1')
+let res2 = document.getElementById('res2')
+let res3 = document.getElementById('res3')
+let res4 = document.getElementById('res4')
+let res5 = document.getElementById('res5')
+let res6 = document.getElementById('res6')
+let res7 = document.getElementById('res7')
+let res8 = document.getElementById('res8')
+let res9 = document.getElementById('res9')
+let res10 = document.getElementById('res10')
+
+function resp1(){
+    res1.innerHTML = "As linguagens mais conhecidas de programação desktop são: Cobol: linguagem de alto nível para a área de negócios; Java: Linguagem rápida e segura para codificar tudo; C#: Linguagem de alto nível que serve para objetos e componentes; JavaScript: Linguagem de alto nível para desenvolvedores web; Python: Linguagem de alto nível que serve para coisas direcionadas para dados, usadas principalmente por cientistas de dados."
+}
+function resp2(){
+    res2.innerHTML = "Java; Kotlin: Linguagem de alto nível que serve como suporte para objetos e funcional; JavaScript; Swift: Linguagem de alto nível que serve para conectar instituições financeiras, e permitir o envio do recebimento do dinheiro; C#."
+}
+function resp3(){
+    res3.innerHTML = "JavaScript; PHP: Linguagem de alto nível serve para gerar conteúdo de alta performance num site; HTML: Linguagem de alto nível que serve para marcação de hipertexto um dos componentes base da web; Python; C#."
+}
+function resp4(){
+    res4.innerHTML = "A front-end dirige para os usuários interagirem e executar alguma ação exemplo para a criação de interfaces, já a back-end tem como propósito a área “interna”, ou seja, a parte estrutural de uma página e a Full stack possui conhecimento e habilidades das duas linguagens, ou seja, ele é capaz de desenvolver aplicações e softwares completos."
+}
+function resp5(){
+    res5.innerHTML = "Os programadores de Front-end utilizam mais as linguagens de programação HTML, CSS e JavaScript. "
+}
+function resp6(){
+    res6.innerHTML = "Os programadores de Back-end utilizam mais as linguagens Python, rails e Java."
+}
+function resp7(){
+    res7.innerHTML = "O pseudocódigo é tipo um rascunho de uma programação ou um algoritmo antes de ser implementado em uma linguagem de programação."
+}
+function resp8(){
+    res8.innerHTML = "O pseudocódigo é usado para escrever códigos como os de programação ou descrever processos e algoritmos usando uma linguagem mais natural."
+}
+function resp9(){
+    res9.innerHTML = "É um bom começo para quem está começando a programar pois facilita e possibilita uma transição mais suave para o mundo das linguagens de programação."
+}
+function resp10(){
+    res10.innerHTML = "VisuAlg, onde sua principal função serve para a interpretação de algoritmos em portugol, Portugol Studio, que serve para fazer programação em inglês."
+    console.log("olá")
+}
---
+++ exemplo1/assets/js/script_variaveis.js
@@ -0,0 +1,7 @@
+function programa() {
+    
+    let y = 10;
+
+    alert("O valor de y: ",y)
+    promt("Você é gay?")
+}
